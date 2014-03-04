e669e9bada5feccdfce078989bc8b1f27a566a21
flavio <fdanesse@gmail.com>
2014-03-04 19:55:03 -0200

    Corrección, Ubuntu Radio en vala utiliza mejor gstreamer 0.10 que la versión 1.0

--- UbuntuRadio/vala/compile.sh
+++ UbuntuRadio/vala/compile.sh
@@ -1,1 +1,1 @@
-valac --pkg gtk+-3.0 --pkg gdk-x11-3.0 --pkg gstreamer-1.0 UbuntuRadio.vala Widgets.vala RadioPlayer.vala
+valac --pkg gtk+-3.0 --pkg gdk-x11-3.0 --pkg gstreamer-0.10 --pkg gstreamer-interfaces-0.10 UbuntuRadio.vala Widgets.vala RadioPlayer.vala
